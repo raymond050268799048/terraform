daed62312180140f48d75bfdee33d0ecbaa72d23
raymond050268799048 <yimwai52@hotmail.com>
2023-04-20 04:21:21 +0800

    azure-k8s

--- azure/main.tf
+++ azure/main.tf
@@ -68,3 +68,39 @@ output "app_id" {
 }
 
 
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "ea2-aks"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "ea2-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+    enable_auto_scaling = true
+    min_count = 2
+    max_count = 4
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  auto_scaler_profile { 
+    max_node_provisioning_time       = "15m"
+    max_unready_nodes                = 3
+    max_unready_percentage           = 45
+  }
+
+
+  
+
+  tags = {
+    environment = "Demo"
+  }
+}
---
+++ azure/terraform.tfvars
@@ -0,0 +1,2 @@
+appId    = "e168380e-301a-477a-973a-a9c81c8bb590"
+password = "q5s8Q~5NHQJDC86zb26rTHejWGTpOHHnsdOu_az_"
